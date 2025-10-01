192bf9cc81d377ca45dc475ea712786ef8f9edda
LuisOtavio <luisotaviomaiabarrientos@gmail.com>
2025-09-30 23:59:40 -0300

    Ultimo commit tentando colocar a api do missao visao e valores que eu tinha esquecido

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,8 +1,10 @@
 // src/app/page.tsx
 import prisma from "@/src/lib/db";
 import HomePage from "@/src/components/HomePage";
+import { getIdentities } from "@/src/lib/api/get-mvv";
 
 export default async function Home() {
+
   const produtosDB = await prisma.product.findMany({
     orderBy: { createdAt: "desc" },
     take: 9, 
@@ -14,6 +16,23 @@ export default async function Home() {
     preco: p.price,
     img_url: "/images/notebook.jpg", 
   }));
-
-  return <HomePage produtos={produtos} />;
+  try {
+    const data = await getIdentities();
+    return (
+      <>
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              <p>{item.title}</p>
+              <p>{item.text}</p>
+            </li>
+          ))}
+        </ul>
+        <HomePage produtos={produtos} />
+      </>
+    );
+  } catch (error) {
+    console.error("Error fetching identities:", error);
+    return <p>Failed to load identities.</p>;
+  }
 }
